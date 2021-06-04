05448ed9add48401760645ab1d4a261168f8d033
Tela Caul <telacaul@gmail.com>
2021-06-04 14:24:53 -0700

    employee test

---
+++ __tests__/employee.test.js
@@ -0,0 +1,27 @@
+const { expect, test } = require('@jest/globals');
+const employee = require('../lib/employee');
+
+test('create new employee object', () => {
+    const employee = new Employee('Tela');
+});
+
+test('get employee ID info', () => {
+    const randomID = 1234;
+    const employee = new Employee('Tela', randomID);
+
+    expect(employee.id).toBe(randomID);
+});
+
+test('get employee email info', () => {
+    const randomEmail = 'random@yahoo.com';
+    const employee = new Employee('Tela', 2265, randomEmail);
+
+    expect(employee.email).toBe(randomEmail);
+})
+
+test('get job role info', () => {
+    const role = 'employee';
+    const employee = new Employee('Tela', 2265, role);
+
+    expect(employee.role).toBe(role);
+})
